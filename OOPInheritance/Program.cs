b89f62737064c3ec60b5277b339d2a09026c74a5
Huan <huanyang.ooi@gmail.com>
2023-10-22 00:59:32 +0200

    cleaned up

--- OOPInheritance/Program.cs
+++ OOPInheritance/Program.cs
@@ -7,17 +7,17 @@
             Bird bird1 = new Bird("Birdo","Yellow","Seeds",2,"Female",false,"Sparrow");
             Bird bird2 = new Bird("Pete", "Black", "Fish", 1, "Male", false, "Penguin");
 
-            bird1.getDescription();
-            bird1.doFlight();
+            //bird1.getDescription();
+            //bird1.doFlight();
             bird1.makeSound();
             Console.WriteLine();
-            bird2.getDescription();
-            bird2.doFlight();
+            //bird2.getDescription();
+            //bird2.doFlight();
             bird2.makeSound();
             Console.WriteLine();
             Sloth sloth1 = new Sloth("Sonic", "brown", "leaves", 3, "Male", false, false);
 
-            sloth1.getDescription();
+            //sloth1.getDescription();
             sloth1.checkSleep();
             sloth1.makeSound();
             sloth1.checkSleep();
@@ -27,21 +27,22 @@
             Poodle poodle1 = new Poodle("Pippi","White","chicken",9,"Female");
             Lagotto lagotto1 = new Lagotto("Lucy","brown","beef",7,"Female");
 
-            dog1.getDescription();
-            dog1.doEat();
-            dog1.goFetch();
+            //dog1.getDescription();
+            //dog1.doEat();
+            //dog1.goFetch();
+            dog1.makeSound();
             Console.WriteLine();
-            poodle1.getDescription();
-            poodle1.doEat();
-            poodle1.goFetch();
+            //poodle1.getDescription();
+            //poodle1.doEat();
+            //poodle1.goFetch();
             poodle1.makeSound();
-            poodle1.doTrick();
+            //poodle1.doTrick();
             Console.WriteLine();
-            lagotto1.getDescription();
-            lagotto1.doEat();
-            lagotto1.goFetch();
+            //lagotto1.getDescription();
+            //lagotto1.doEat();
+            //lagotto1.goFetch();
             lagotto1.makeSound();
-            lagotto1.findTruffle();
+            //lagotto1.findTruffle();
             Console.WriteLine();
         }
     }
